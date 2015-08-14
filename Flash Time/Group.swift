efcea35e95cc64e9e82f04dafb83ea757a4b850e
cpwhidden <Christopher.Whidden@gmail.com>
2015-08-13 22:55:06 -0500

    feat: Cards belong in groups

---
+++ Flash Time/Group.swift
@@ -0,0 +1,29 @@
+//
+//  Group.swift
+//  Flash Time
+//
+//  Created by Christopher Whidden on 8/13/15.
+//  Copyright (c) 2015 SelfEdge Software. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Group: NSManagedObject {
+
+    @NSManaged var name: String
+    @NSManaged var cards: NSSet?
+    @NSManaged var defaultConfiguration: Configuration
+
+    init(name: String, cards: NSSet? = [], defaultConfiguration: Configuration, context: NSManagedObjectContext) {
+        let entity = NSEntityDescription.entityForName("Group", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        self.name = name
+        self.cards = cards
+        self.defaultConfiguration = defaultConfiguration
+    }
+    
+    convenience init(name: String, cards: NSSet? = [], defaultConfiguration: Configuration) {
+        self.init(name: name, cards: cards, defaultConfiguration: defaultConfiguration, context: sharedContext())
+    }
+}
