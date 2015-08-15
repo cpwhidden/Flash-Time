cac00a4cd8ceebcce397caf27f88c173425283a0
cpwhidden <Christopher.Whidden@gmail.com>
2015-08-14 22:00:01 -0500

    feat: Setup EditGroupViewController
    Add delegate methods for components
    Add Core Data calls for configurations
    and group creation/update

--- Flash Time/EditGroupViewController.swift
+++ Flash Time/EditGroupViewController.swift
@@ -8,23 +8,70 @@
 
 import UIKit
 
-class EditGroupViewController: UIViewController {
+class EditGroupViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var configurationPicker: UIPickerView!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
     var group: Group?
+    var configurations: [Configuration]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        configurations = sharedContext.fetchAllOfEntity("Configuration") as! [Configuration]
+        
+        if let group = group {
+            nameTextField.text = group.name
+            let index = find(configurations, group.defaultConfiguration)!
+            configurationPicker.selectRow(index, inComponent: 0, animated: true)
+        }
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let finalString = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string)
+        if count(finalString) != 0 {
+            doneButton.enabled = true
+        } else {
+            doneButton.enabled = false
+        }
+        return true
+    }
+    
+    @IBAction func doneTapped(sender: UIBarButtonItem) {
+        let index = configurationPicker.selectedRowInComponent(0)
+        if let group = group {
+            group.name = nameTextField.text
+            group.defaultConfiguration = configurations[index]
+        } else {
+            let group = Group(name: nameTextField.text, cards: [], defaultConfiguration: configurations[index])
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func cancelTapped(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    // MARK: - Picker View Data Source
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return configurations.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return configurations[row].name
+    }
+    
     /*
     // MARK: - Navigation
 
