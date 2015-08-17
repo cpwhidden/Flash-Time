e682f025143477b0f650f0178b92042baf78a793
cpwhidden <Christopher.Whidden@gmail.com>
2015-08-16 20:48:25 -0500

    refactor: View controller is now the delegate

--- Flash Time/TextTableViewCell.swift
+++ Flash Time/TextTableViewCell.swift
@@ -8,28 +8,18 @@
 
 import UIKit
 
-class TextTableViewCell: UITableViewCell, UITextViewDelegate {
+class TextTableViewCell: UITableViewCell {
     @IBOutlet weak var textView: UITextView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        textView.delegate = self
     }
     
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
-    }
-    
-    func textViewDidBeginEditing(textView: UITextView) {
-        textView.becomeFirstResponder()
-    }
-    
-    func textViewDidEndEditing(textView: UITextView) {
-        textView.resignFirstResponder()
     }
 
 }
